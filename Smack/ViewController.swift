f2290b79e6de170b055a1f555f676934e3d3893f
Volha Stankevich <stankevichvolhaig@gmail.com>
2019-06-26 10:14:32 -0700

    Start Smack

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Volha  Stankevich on 6/26/19.
+//  Copyright © 2019 Volha  Stankevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
